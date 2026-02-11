25ac0869e5fbd294df57874d46ae932d9089a67d
kami77dev <kamilafernandessilva78@gmail.com>
2026-02-11 23:22:25 +0000

    atualizacao

---
+++ aula-00-conteudo/ex01/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { AppRegistry, View, Text, StyleSheet} from "react-native";
+import OlaMundo from "./components/OlaMundo";
+
+export default function App(){
+  return (
+    <View style={styles.container}>
+    <OlaMundo/>
+    </View>
+  );
+
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1, 
+    justifyContent: 'center',
+    alignItems:'center',
+    backgroundColor:'#4de7ec',
+  },
+
+  text:{
+    fontZise:24,
+    fontWeight:'bold',
+  },
+
+});
---
+++ aula-00-conteudo/ex01/components/OlaMundo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function OlaMundo(){
+    return(
+        <View>
+        <Text>Olá</Text>
+        <text>Mundo</text>
+        </View>
+    );
+}
