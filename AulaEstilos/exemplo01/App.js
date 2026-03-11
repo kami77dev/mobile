bcad5bfcf235f8fab7648a3602393c68ca85209e
kami77dev <kamilafernandessilva78@gmail.com>
2026-03-11 23:35:07 +0000

    att

---
+++ AulaEstilos/exemplo01/App.js
@@ -0,0 +1,18 @@
+import{Text, View} from 'react-native'
+
+export default function App(){
+  return(
+
+    <view style = {{
+      flex:1,
+      justifyContent:'center',
+      alignItems: 'center',
+      backgroundColor:  '#f5f5f5'
+    }}>
+      <text style={{{
+        
+      }}}></text>
+
+    </view>
+  );
+}
